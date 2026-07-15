6c90b56b9ccb0dfcda950a6592f42e30208b98ba
Hritika <hritikagupta2345@gmail.com>
2026-07-16 02:45:46 +0530

    Gemini vision API working - image description

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
+from dotenv import load_dotenv
+from google import genai
+from PIL import Image
 st.title("Attendance Tracker: ")
 tt=st.file_uploader("Upload Timetable :" \
 "", type=["png", "jpg", "jpeg"])
-if tt:
-    st.image(tt)
-from dotenv import load_dotenv
 import os
 load_dotenv()
 api=os.getenv("GOOGLE_API_KEY")
+if tt:
+    st.image(tt)
+    client=genai.Client(api_key=api)
+    img=Image.open(tt)
+    response=client.models.generate_content(model="gemini-flash-latest", contents=[img, "describe the image"])
+    st.write(response.text)
