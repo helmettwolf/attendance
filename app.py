ec250210cdee94700256e8b1ad02c2570c17845d
Hritika <hritikagupta2345@gmail.com>
2026-07-25 19:23:22 +0530

    json loading

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 from PIL import Image
+import json
 st.title("Attendance Tracker: ")
 tt=st.file_uploader("Upload Timetable :" \
 "", type=["png", "jpg", "jpeg"])
@@ -12,5 +13,7 @@ if tt:
     st.image(tt)
     client=genai.Client(api_key=api)
     img=Image.open(tt)
-    response=client.models.generate_content(model="gemini-flash-latest", contents=[img, "describe the image"])
-    st.write(response.text)
+    prompt = "Extract this timetable as JSON. For each class, include: week, day, period, subject. Return ONLY valid JSON, no explanation, no markdown formatting."
+    response=client.models.generate_content(model="gemini-flash-lite-latest", contents=[img, prompt])
+    data=json.loads(response.text)
+    st.write(data)
